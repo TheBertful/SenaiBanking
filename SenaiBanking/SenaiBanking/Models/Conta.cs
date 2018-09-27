a98b619acfb5ed33534959122009ddee24d5e742
TheBertful <robertosmoure@gmail.com>
2018-09-27 10:08:35 -0300

    Criando classes Model
    Cliente, conta, contacorrente

---
+++ SenaiBanking/SenaiBanking/Models/Conta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SenaiBanking.Models
+{
+    public class Conta
+    {
+        public int Id { get; set; }
+        public Cliente ClienteProp { get; set; }
+        public int Numero { get; set; }
+        public string Senha { get; set; }
+        public double Saldo { get; set; }
+    }
+}
