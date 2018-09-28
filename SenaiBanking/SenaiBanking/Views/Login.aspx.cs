e77489593540248a7034cb818123a4abb3bafe92
tsubusu <43643091+tsubusu@users.noreply.github.com>
2018-09-28 14:36:37 -0300

    Alteracao nas telas de: Login, Principal, Sacar, Saldo
    Login: Foi adicionado as sessões de cliente e conta corrente, também foi criada uma autenticação de usuario.
    Principal: Esta exibindo o nome do cliente.
    Sacar: Foi implementado as funções.
    Saldo: Foi implementado as funções

--- SenaiBanking/SenaiBanking/Views/Login.aspx.cs
+++ SenaiBanking/SenaiBanking/Views/Login.aspx.cs
@@ -11,31 +11,42 @@ namespace SenaiBanking.Views
     public partial class Login : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {   
+        {
+            txtMsg.Visible = false;
             Cliente cliente = new Cliente()
             {
                 Id = 1,
                 Nome = "Usuario",
-                Cpf = "00000000000",
+                //Cpf = "00000000000",
+                Cpf = "0",
                 Senha = "123"
             };
             Session["Cliente"] = cliente;
-
-            Session["ContaCorrente"] = new ContaCorrente()
-            {
-                Id = 1,
-                ClienteProp = cliente,
-                Limite = 1000.00,
-                Numero = 1,
-                Saldo = 1500.00,
-                Tipo = "Gold",
-            };
-
         }
 
         protected void btnEntrar_Click(object sender, EventArgs e)
         {
+            Cliente cliente = Session["Cliente"] as Cliente;
+            if(cliente.Cpf.Equals(txtCpf.Text) && cliente.Senha.Equals(txtSenha.Text))
+            {
+                Session["Cliente"] = cliente;
+                Session["ContaCorrente"] = new ContaCorrente()
+                {
+                    Id = 1,
+                    ClienteProp = cliente,
+                    Limite = 1000.00,
+                    Numero = 1,
+                    Saldo = 1500.00,
+                    Tipo = "Gold",
+                };
 
+                Response.Redirect("~/Views/Principal.aspx");
+            }
+            else
+            {
+                txtMsg.Visible = true;
+                txtMsg.Text = "Login ou senha inválidos";
+            }
         }
     }
 }
